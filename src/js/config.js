e82591d6225174af97774388f3457a2927679790
jonnohuang <jonnohuang@hotmail.com>
2020-12-14 14:18:01 -0800

    Updated Forkify API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'e0792a44-c773-45cf-ac49-f36fe3da8f39';
 export const MODAL_CLOSE_SEC = 2.5;
